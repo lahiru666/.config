ea37033610ac0d50da5179e31cb33b03e2a2c1ba
Lahiru Priyankara <74791955+lahiru666@users.noreply.github.com>
2026-06-15 11:07:38 +0530

    Update vim.sh to change installation instructions

--- vim.sh
+++ vim.sh
@@ -6,5 +6,5 @@ mkdir -p ~/.vim/autoload
 curl -fLs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim > ~/.vim/autoload/plug.vim
 touch ~/.vimrc
 echo -e "call plug#begin('~/.vim/plugged')\nPlug 'neoclide/coc.nvim', {'branch': 'release'}\nPlug 'preservim/nerdtree'\nPlug 'vim-airline/vim-airline'\nPlug 'joshdick/onedark.vim'\ncall plug#end()\nset number\nset mouse=a\nset cursorline\nhighlight Cursorline cterm=bold ctermbg=black\nsyntax on\nset hlsearch\nset ignorecase\nset smartcase\nset showmatch\ncolorscheme onedark" > ~/.vimrc
-echo "after runnig this script you should run CocInstall coc-sh in vim command mode"
+echo "after runnig this script you should run :PlugInstall in vim command mode"
 
